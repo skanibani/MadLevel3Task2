6212f23d3f2ca07381a1cbd61d9c624c7fe9e7ae
Skander <skander@bibani.nl>
2020-11-06 18:40:35 +0100

    Add PortalAdapter

---
+++ app/src/main/java/com/example/madlevel3task2/PortalAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.madlevel3task2
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_portal.view.*
+
+class PortalAdapter(private val portals: List<Portal>): RecyclerView.Adapter<PortalAdapter.ViewHolder>() {
+
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+        fun databind(portal: Portal) {
+            itemView.portalTitle.text = portal.name
+            itemView.portalURL.text = portal.url
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+                LayoutInflater.from(parent.context).inflate(R.layout.item_portal, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.databind(portals[position])
+    }
+
+    override fun getItemCount(): Int {
+        return portals.size
+    }
+}
